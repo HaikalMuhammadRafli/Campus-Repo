6a2e9712806cccbd09c64d559adc00c38eeb8ca7
Haikal Muhammad Rafli <haikalrafli0020@gmail.com>
2023-10-09 14:55:16 +0700

    added DoWhile14 file

---
+++ Semester 1/Week 7/DoWhile14.java
@@ -0,0 +1,13 @@
+public class DoWhile14 {
+
+    public static void main(String[] args) {
+
+        int x = 0;
+
+        do {
+            System.out.println(x);
+        } while (++x <= 8);
+
+        System.out.println("Program berhenti!");
+    }
+}

65fbb669ec2f05353da0b7062cd0038b37ffbad9
Haikal Muhammad Rafli <haikalrafli0020@gmail.com>
2023-10-11 11:29:18 +0700

    added TeoriMandiriA14 file

---
+++ Semester 1/Week 7/TeoriMandiriA14.java
@@ -0,0 +1,13 @@
+public class TeoriMandiriA14 {
+
+    public static void main(String[] args) {
+
+        int total = 0;
+
+        for (int i = 25; i >= 1; i--) {
+            total += i;
+        }
+
+        System.out.println("Total : " + total);
+    }
+}

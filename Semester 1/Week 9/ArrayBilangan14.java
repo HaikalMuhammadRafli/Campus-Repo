7794d47d7217a1323289d4952372f2a052ff89b9
Haikal Muhammad Rafli <haikalrafli0020@gmail.com>
2023-10-23 09:27:38 +0700

    added ArrayBilangan14 file

---
+++ Semester 1/Week 9/ArrayBilangan14.java
@@ -0,0 +1,20 @@
+
+public class ArrayBilangan14 {
+
+    public static void main(String[] args) {
+        // deklarasi array bertipe integer
+        int[] bil = new int[4];
+
+        // menginputkan value ke array
+        bil[0] = 5;
+        bil[1] = 13;
+        bil[2] = -7;
+        bil[3] = 17;
+
+        // menampilkan isi array satu per satu
+        System.out.println(bil[0]);
+        System.out.println(bil[1]);
+        System.out.println(bil[2]);
+        System.out.println(bil[3]);
+    }
+}

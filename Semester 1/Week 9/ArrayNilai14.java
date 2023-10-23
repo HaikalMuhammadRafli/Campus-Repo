4150c365f879f8e459ae00238ec5b1b40b1d2e89
Haikal Muhammad Rafli <haikalrafli0020@gmail.com>
2023-10-23 10:06:32 +0700

    added ArrayNilai14 file

---
+++ Semester 1/Week 9/ArrayNilai14.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ArrayNilai14 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // deklarasi array integer nilaiAkhir
+        int[] nilaiAkhir = new int[10];
+
+        // looping untuk menginput elemen array
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Masukkan nilai akhir ke-" + i + " : ");
+            nilaiAkhir[i] = sc.nextInt();
+        }
+
+        // looping untuk menampilkan elemen-elemen array
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Nilai akhir ke-" + i + " adalah " + nilaiAkhir[i]);
+        }
+    }
+}

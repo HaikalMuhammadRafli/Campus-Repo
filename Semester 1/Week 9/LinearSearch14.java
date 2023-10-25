f38f154bbf2a2d50c1c39cfa7c6dbd84921f92ad
Haikal Muhammad Rafli <haikalrafli0020@gmail.com>
2023-10-25 10:47:00 +0700

    added percobaan 4 pertanyaan 3 mod

--- Semester 1/Week 9/LinearSearch14.java
+++ Semester 1/Week 9/LinearSearch14.java
@@ -10,6 +10,7 @@ public class LinearSearch14 {
         // deklarasi variable dan array
         int key, hasil = 0, jmlElemen, input;
         int[] arrayInt;
+        boolean found = false;
 
         // input jumlah elemen yang di dalam array
         System.out.print("Masukkan jumlah elemen array : ");
@@ -38,13 +39,20 @@ public class LinearSearch14 {
             if (arrayInt[i] == key) {
                 hasil = i;
 
+                // jika ditemukan, maka boolean found akan menjadi true
+                found = true;
+
                 // kemudian looping di break
                 break;
             }
         }
 
-        // menampilkan hasil
-        System.out.println("Key ada dalam array pada posisi indeks ke-" + hasil);
+        // mengecek apakah key ditemukan atau tidak menggunakan validasi boolean
+        if (found != true) {
+            System.out.println("Key tidak ditemukan!");
+        } else {
+            System.out.println("Key ada dalam array pada posisi indeks ke-" + hasil);
+        }
 
         sc14.close();
     }

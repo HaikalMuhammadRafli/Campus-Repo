a15c18d4b9e5bdd580866774386a03e5b45b3059
Haikal Muhammad Rafli <haikalrafli0020@gmail.com>
2023-10-25 11:12:49 +0700

    added Latihan_Individu_114 file

---
+++ Semester 1/Week 9/Latihan_Individu_114.java
@@ -0,0 +1,15 @@
+public class Latihan_Individu_114 {
+
+    public static void main(String[] args) {
+
+        int[] bil = new int[60];
+
+        for (int i = 0; i < bil.length; i++) {
+            bil[i] = i + 1;
+        }
+
+        for (int i = 0; i < bil.length; i++) {
+            System.out.println(bil[i]);
+        }
+    }
+}

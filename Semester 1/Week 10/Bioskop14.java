01d541deb922df1f21744593e1f0414fe20e263d
Haikal Muhammad Rafli <haikalrafli0020@gmail.com>
2023-10-30 13:13:32 +0700

    added pertanyaan 12 mod

--- Semester 1/Week 10/Bioskop14.java
+++ Semester 1/Week 10/Bioskop14.java
@@ -5,6 +5,7 @@ public class Bioskop14 {
 
         String[][] penonton = new String[4][2];
 
+        // memberikan nilai pada setiap indeks column dan row
         penonton[0][0] = "Amin";
         penonton[0][1] = "Bena";
         penonton[1][0] = "Candra";
@@ -14,12 +15,15 @@ public class Bioskop14 {
         penonton[3][0] = "Gisel";
         penonton[3][1] = "Hana";
 
+        // mengoutputkan panjang indeks array penonton
         System.out.println(penonton.length);
 
+        // looping untuk mengoutputkan panjang elemen di setiap row
         for (String[] barisPenonton : penonton) {
             System.out.println("Panjang baris : " + barisPenonton.length);
         }
 
+        // looping untuk mengoutputkan penonton di setiap row
         for (int i = 0; i < penonton.length; i++) {
             System.out.println("Penonton pada baris ke-" + (i + 1) + " : " + String.join(" ,", penonton[i]));
         }

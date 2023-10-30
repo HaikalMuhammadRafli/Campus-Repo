41b4bcf6abf3387eeab2862c3ce26092ddb3dcd7
Haikal Muhammad Rafli <haikalrafli0020@gmail.com>
2023-10-30 12:15:25 +0700

    added pertanyaan 10 mod

--- Semester 1/Week 10/Bioskop14.java
+++ Semester 1/Week 10/Bioskop14.java
@@ -19,5 +19,10 @@ public class Bioskop14 {
         for (String[] barisPenonton : penonton) {
             System.out.println("Panjang baris : " + barisPenonton.length);
         }
+
+        System.out.println("Penonton pada garis ke-3");
+        for (int i = 0; i < penonton[2].length; i++) {
+            System.out.println(penonton[2][i]);
+        }
     }
 }

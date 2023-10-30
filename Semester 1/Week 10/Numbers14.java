84377b81254c63fda3cb11093a075802e2b021e5
Haikal Muhammad Rafli <haikalrafli0020@gmail.com>
2023-10-30 15:03:37 +0700

    added Numbers14 file

---
+++ Semester 1/Week 10/Numbers14.java
@@ -0,0 +1,16 @@
+public class Numbers14 {
+
+    public static void main(String[] args) {
+
+        // deklarasi dan instansi array
+        int[][] myNumbers = new int[3][];
+
+        // instansi kolom baris ke 0 meiliki panjang elemen 5
+        myNumbers[0] = new int[5];
+        // instansi kolom baris ke 1 meiliki panjang elemen 3
+        myNumbers[1] = new int[3];
+        // instansi kolom baris ke 2 meiliki panjang elemen 1
+        myNumbers[2] = new int[1];
+
+    }
+}

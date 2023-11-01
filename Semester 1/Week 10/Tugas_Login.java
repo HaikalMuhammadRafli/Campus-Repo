12bc96f000591c417417cd6d5fec464ba397668e
Haikal Muhammad Rafli <haikalrafli0020@gmail.com>
2023-11-01 10:55:16 +0700

    fix array name

--- Semester 1/Week 10/Tugas_Login.java
+++ Semester 1/Week 10/Tugas_Login.java
@@ -7,7 +7,7 @@ public class Tugas_Login {
         // deklarasi dan inisialisasi scanner, variable, dan array
         Scanner sc = new Scanner(System.in);
 
-        String[][] user = { { "haikal", "123" }, { "irsyad", "456" }, { "esa", "789" } };
+        String[][] users = { { "haikal", "123" }, { "irsyad", "456" }, { "esa", "789" } };
         String inputUS, inputPW;
         int id = 0;
         boolean access = false;
@@ -25,9 +25,9 @@ public class Tugas_Login {
             inputPW = sc.nextLine();
 
             // looping search user dan password
-            for (int i = 0; i < user.length; i++) {
+            for (int i = 0; i < users.length; i++) {
                 // jika true, maka id bernilai index dan access bernilai true
-                if (inputUS.equals(user[i][0]) && inputPW.equals(user[i][1])) {
+                if (inputUS.equals(users[i][0]) && inputPW.equals(users[i][1])) {
                     System.out.println();
                     System.out.println("Login berhasil!");
                     id = i;
@@ -46,7 +46,7 @@ public class Tugas_Login {
         } while (access != true);
 
         // output berhasil login
-        System.out.println("Selamat datang " + user[id][0] + "!");
+        System.out.println("Selamat datang " + users[id][0] + "!");
         sc.close();
 
     }

15dc1f1c2d6f3d1966b211aa84b542322933ce36
Haikal Muhammad Rafli <haikalrafli0020@gmail.com>
2023-11-06 13:32:02 +0700

    added Quiz14 file

---
+++ Semester 1/Week 11/Quiz14.java
@@ -0,0 +1,38 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Quiz14 {
+
+    public static void main(String[] args) {
+
+        // deklarasi random dan scanner
+        Random random = new Random();
+        Scanner input = new Scanner(System.in);
+
+        // deklarasi dan inisialisasi variable menu
+        char menu = 'y';
+
+        // outer loop
+        do {
+            // deklarasi dan inisialisasi dengan menggunakan bilangan random + 1 agar tidak
+            // mendapat bilangan 0 sebagai jawaban
+            int number = random.nextInt(10) + 1;
+            // deklarasi bool success
+            boolean success = false;
+            do {
+                // input answer
+                System.out.print("Tebak angka (1-10): ");
+                int answer = input.nextInt();
+                input.nextLine();
+                // jika jawaban == number maka success akan bernilai true
+                success = (answer == number);
+
+                // jika belum true maka akan mengulang
+            } while (!success);
+            System.out.print("Apakah Anda ingin mengulang permainan (Y/y)?");
+            menu = input.nextLine().charAt(0);
+        } while (menu == 'y' || menu == 'Y');
+
+        input.close();
+    }
+}

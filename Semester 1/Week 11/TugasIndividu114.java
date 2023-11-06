e4cafd5077ecbc18f6dbf2e5ed43c003ffc30b6a
Haikal Muhammad Rafli <haikalrafli0020@gmail.com>
2023-11-06 14:59:43 +0700

    fix: minimal 3

--- Semester 1/Week 11/TugasIndividu114.java
+++ Semester 1/Week 11/TugasIndividu114.java
@@ -8,21 +8,25 @@ public class TugasIndividu114 {
         Scanner sc = new Scanner(System.in);
 
         // input jumlah baris dan kolom
-        System.out.print("Masukkan nilai N : ");
+        System.out.print("Masukkan nilai N (minimal 3): ");
         int N = sc.nextInt();
 
         // outer loop baris
-        for (int i = 1; i <= N; i++) {
-            // inner loop untuk spasi, jika jumlahnya 5 maka akan dikurangi i disetiap baris
-            for (int j = 1; j <= (N - i); j++) {
-                System.out.print(" ");
+        if (N >= 3) {
+            for (int i = 1; i <= N; i++) {
+                // inner loop untuk spasi, jika jumlahnya 5 maka akan dikurangi i disetiap baris
+                for (int j = 1; j <= (N - i); j++) {
+                    System.out.print(" ");
+                }
+                // inner loop angka
+                for (int j = 1; j <= i; j++) {
+                    System.out.print(j);
+                }
+                // new line
+                System.out.println();
             }
-            // inner loop angka
-            for (int j = 1; j <= i; j++) {
-                System.out.print(j);
-            }
-            // new line
-            System.out.println();
+        } else {
+            System.out.println("error: Nilai N minimal 3");
         }
 
         sc.close();

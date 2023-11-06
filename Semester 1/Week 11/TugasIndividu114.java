42932f65c52d7d80eb86362311dc861f7dec1d82
Haikal Muhammad Rafli <haikalrafli0020@gmail.com>
2023-11-06 14:51:52 +0700

    fix: added comments

--- Semester 1/Week 11/TugasIndividu114.java
+++ Semester 1/Week 11/TugasIndividu114.java
@@ -4,18 +4,24 @@ public class TugasIndividu114 {
 
     public static void main(String[] args) {
 
+        // deklarasi scanner
         Scanner sc = new Scanner(System.in);
 
+        // input jumlah baris dan kolom
         System.out.print("Masukkan nilai N : ");
         int N = sc.nextInt();
 
+        // outer loop baris
         for (int i = 1; i <= N; i++) {
+            // inner loop untuk spasi, jika jumlahnya 5 maka akan dikurangi i disetiap baris
             for (int j = 1; j <= (N - i); j++) {
                 System.out.print(" ");
             }
+            // inner loop angka
             for (int j = 1; j <= i; j++) {
                 System.out.print(j);
             }
+            // new line
             System.out.println();
         }
 

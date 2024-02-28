f1ef61cb15601d6361215d1ace54f7eee9dd6167
Haikal Muhammad Rafli <haikalrafli0020@gmail.com>
2024-02-28 09:50:45 +0700

    add: LimasSegiEmpat.java

---
+++ Semester 2/ALSTRUD/Week 3/src/LimasSegiEmpat.java
@@ -0,0 +1,12 @@
+public class LimasSegiEmpat {
+
+    double panjangAlas, tinggi;
+
+    public double getLuasPermukaan() {
+        return Math.pow(panjangAlas, 2) + 4 * (panjangAlas * tinggi / 2);
+    }
+
+    public double getVolume() {
+        return Math.pow(panjangAlas, 2) * tinggi / 3;
+    }
+}

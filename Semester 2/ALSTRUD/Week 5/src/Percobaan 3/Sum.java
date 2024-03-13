988d7be9e6dcc151b21a301a464392552600bb33
Haikal Muhammad Rafli <haikalrafli0020@gmail.com>
2024-03-13 13:12:06 +0700

    fix: percobaan 3 bugs

---
+++ Semester 2/ALSTRUD/Week 5/src/Percobaan 3/Sum.java
@@ -0,0 +1,33 @@
+
+public class Sum {
+    int elemen;
+    double keuntungan[], total;
+
+    Sum(int elemen) {
+        this.elemen = elemen;
+        this.keuntungan = new double[elemen];
+        this.total = 0;
+    }
+
+    double totalBF(double arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            total = total + arr[i];
+        }
+        return total;
+    }
+
+    double totalDC(double arr[], int l, int r) {
+        if (l == r) {
+            return arr[l];
+
+        } else if (l < r) {
+            int mid = (r + l) / 2;
+            double lsum = totalDC(arr, l, mid);
+            double rsum = totalDC(arr, mid + 1, r);
+
+            return lsum + rsum;
+        }
+
+        return 0;
+    }
+}

24f197203e2ba118f02ab4e6b2b984e6810303fa
Haikal Muhammad Rafli <haikalrafli0020@gmail.com>
2024-03-13 11:20:24 +0700

    fix: percobaan bug

--- Semester 2/ALSTRUD/Week 5/src/MainFaktorial.java
+++ Semester 2/ALSTRUD/Week 5/src/MainFaktorial.java
@@ -9,21 +9,21 @@ public class MainFaktorial {
         System.out.print("Masukkan jumlah elemen : ");
         int iJml = sc.nextInt();
 
-        Faktorial fk[] = new Faktorial[10];
-        for (int i = 0; i < iJml; i++) {
+        Faktorial fk[] = new Faktorial[iJml];
+        for (int i = 0; i < fk.length; i++) {
             fk[i] = new Faktorial();
             System.out.print("Masukkan nilai data ke-" + (i + 1) + " : ");
-            int iNilai = sc.nextInt();
+            fk[i].nilai = sc.nextInt();
         }
 
         System.out.println("HASIL - BRUTE FORCE");
-        for (int i = 0; i < iJml; i++) {
+        for (int i = 0; i < fk.length; i++) {
             System.out.println(
                     "Hasil penghitungan faktorial menggunakan Brute Force adalah : " + fk[i].faktorialBF(fk[i].nilai));
         }
 
         System.out.println("HASIL - DIVIDE AND CONQUER");
-        for (int i = 0; i < iJml; i++) {
+        for (int i = 0; i < fk.length; i++) {
             System.out.println("Hasil penghitungan faktorial menggunakan Divide and Conquer adalah : "
                     + fk[i].faktorialDC(fk[i].nilai));
         }

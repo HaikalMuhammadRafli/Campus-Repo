4744ce82a7a4aded86bcee0b5878c86ed1ab9765
Haikal Muhammad Rafli <haikalrafli0020@gmail.com>
2024-03-20 12:21:21 +0700

    mod: pertanyaan 4

--- Semester 2/ALSTRUD/Week 5/src/Percobaan 2/MainPangkat.java
+++ Semester 2/ALSTRUD/Week 5/src/Percobaan 2/MainPangkat.java
@@ -21,16 +21,32 @@ public class MainPangkat {
             png[i] = new Pangkat(nilai, pangkat);
         }
 
-        System.out.println("HASIL PANGKAT - BRUTE FORCE");
-        for (int i = 0; i < png.length; i++) {
-            System.out.println("Hasil dari " + png[i].nilai + " pangkat " + png[i].pangkat + " adalah "
-                    + png[i].pangkatBf(png[i].nilai, png[i].pangkat));
-        }
+        System.out.println("\nPilih Metode Perhitungan");
+        System.out.println("------------------------");
+        System.out.println("[1] BRUTE FORCE");
+        System.out.println("[2] DIVIDE CONQUER");
+        System.out.print("Pilihan anda : ");
 
-        System.out.println("HASIL PANGKAT - DIVIDE CONQUER");
-        for (int i = 0; i < png.length; i++) {
-            System.out.println("Hasil dari " + png[i].nilai + " pangkat " + png[i].pangkat + " adalah "
-                    + png[i].pangkatDc(png[i].nilai, png[i].pangkat));
+        switch (sc.nextInt()) {
+            case 1:
+                System.out.println("HASIL PANGKAT - BRUTE FORCE");
+                for (int i = 0; i < png.length; i++) {
+                    System.out.println("Hasil dari " + png[i].nilai + " pangkat " + png[i].pangkat + " adalah "
+                            + png[i].pangkatBf(png[i].nilai, png[i].pangkat));
+                }
+                break;
+
+            case 2:
+                System.out.println("HASIL PANGKAT - DIVIDE CONQUER");
+                for (int i = 0; i < png.length; i++) {
+                    System.out.println("Hasil dari " + png[i].nilai + " pangkat " + png[i].pangkat + " adalah "
+                            + png[i].pangkatDc(png[i].nilai, png[i].pangkat));
+                }
+                break;
+
+            default:
+                System.out.println("Invalid choice!");
+                break;
         }
 
         sc.close();

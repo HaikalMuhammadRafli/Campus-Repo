cd507429fd2f0c446824fbe2883474b565ece22a
Haikal Muhammad Rafli <haikalrafli0020@gmail.com>
2024-04-03 13:12:20 +0700

    add: PostfixMain14.java

---
+++ Semester 2/ALSTRUD/Week 8/src/PostfixMain14.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class PostfixMain14 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String P, Q;
+
+        System.out.print("Masukkan ekspresi matematika (infix) : ");
+        Q = sc.nextLine();
+        Q = Q.trim();
+        Q = Q + ")";
+
+        int total = Q.length();
+
+        Postfix14 post = new Postfix14(total);
+        P = post.konversi(Q);
+        System.out.println("Postfix : " + P);
+
+        sc.close();
+    }
+}

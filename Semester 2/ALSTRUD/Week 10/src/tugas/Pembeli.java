082192e7c71cfccc17b6a6589f20ef2de4f5ffc4
Haikal Muhammad Rafli <haikalrafli0020@gmail.com>
2024-04-30 19:55:50 +0700

    add: Pembeli.java

---
+++ Semester 2/ALSTRUD/Week 10/src/tugas/Pembeli.java
@@ -0,0 +1,14 @@
+package tugas;
+
+public class Pembeli {
+    String nama;
+    int noHP;
+
+    Pembeli(String nama, int noHP) {
+        this.nama = nama;
+        this.noHP = noHP;
+    }
+
+    Pembeli() {
+    }
+}

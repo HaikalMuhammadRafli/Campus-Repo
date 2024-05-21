9b84e242a9687320bcae2ae8d7be5871301c2eb5
Haikal Muhammad Rafli <haikalrafli0020@gmail.com>
2024-05-21 22:00:28 +0700

    add: Mahasiswa.java

---
+++ Semester 2/ALSTRUD/Week 11/src/Tugas1/Mahasiswa.java
@@ -0,0 +1,11 @@
+public class Mahasiswa {
+    int nim;
+    String nama;
+    Mahasiswa next;
+
+    Mahasiswa(int nim, String nama, Mahasiswa next) {
+        this.nim = nim;
+        this.nama = nama;
+        this.next = next;
+    }
+}

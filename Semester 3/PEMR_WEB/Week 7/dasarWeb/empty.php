25baa4103f7cd4fcbc33b79b11771859f1a57657
Haikal Muhammad Rafli <haikalrafli0020@gmail.com>
2024-10-09 10:19:39 +0700

    add: practicum 2 empty form

---
+++ Semester 3/PEMR_WEB/Week 7/dasarWeb/empty.php
@@ -0,0 +1,18 @@
+<?php
+// part 1
+$myArray = array();
+
+if (empty($myArray)) {
+    echo "Array tidak terdefinisi atau kosong.";
+} else {
+    echo "Array terdefinisi dan tidak kosong.";
+}
+
+echo "<br><br>";
+
+// part 2
+if (empty($nonExistentVar)) {
+    echo "Array tidak terdefinisi atau kosong.";
+} else {
+    echo "Array terdefinisi dan tidak kosong.";
+}

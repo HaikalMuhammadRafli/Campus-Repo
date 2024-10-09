d9b89209b09e869e7eba7d3fcaa49d0f3a59e214
Haikal Muhammad Rafli <haikalrafli0020@gmail.com>
2024-10-09 10:09:19 +0700

    add: practicum 1 isset form

---
+++ Semester 3/PEMR_WEB/Week 7/dasarWeb/isset.php
@@ -0,0 +1,19 @@
+<?php
+// part 1
+$umur;
+
+if (isset($umur) && $umur >= 18) {
+    echo "Anda sudah dewasa.";
+} else {
+    echo "Anda belum dewasa atau variable 'umur' tidak ditemukan.";
+}
+echo "<br><br>";
+
+//part 2
+$data = array("nama" => "Jane", "usia" => 25);
+
+if (isset($data["nama"])) {
+    echo "Nama : " . $data["nama"];
+} else {
+    echo "Variable 'nama' tidak ditemukan dalam array.";
+}

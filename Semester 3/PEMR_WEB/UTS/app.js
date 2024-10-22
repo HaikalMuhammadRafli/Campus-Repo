6ae9b7d0a72eb17cae08979c256978d084df2efe
Haikal Muhammad Rafli <haikalrafli0020@gmail.com>
2024-10-22 09:21:29 +0700

    feat: removed unused semi-colons

--- Semester 3/PEMR_WEB/UTS/app.js
+++ Semester 3/PEMR_WEB/UTS/app.js
@@ -1,41 +1,42 @@
 $(document).ready(function () {
-    let selectedPage = 'controllers/index.php';
+    let selectedPage = 'controllers/index.php'
 
     loadPage(selectedPage);
 
     $(document).on('click', '#home-btn', function () {
-        selectedPage = 'controllers/index.php';
+        selectedPage = 'controllers/index.php'
         loadPage(selectedPage);
     });
 
     $(document).on('click', '#create-btn', function () {
-        selectedPage = 'views/create.php';
-        loadPage(selectedPage);
+        selectedPage = 'views/create.php'
+        loadPage(selectedPage)
     });
 
     $(document).on('click', '.edit-btn', function () {
-        const noteId = $(this).data('id');
-        selectedPage = 'controllers/edit.php?id=' + noteId;
-        loadPage(selectedPage);
+        const noteId = $(this).data('id')
+        selectedPage = 'controllers/edit.php?id=' + noteId
+        loadPage(selectedPage)
     });
 
     $(document).on('click', '.delete-btn', function () {
-        const noteId = $(this).data('id');
+        const noteId = $(this).data('id')
         $.ajax({
             url: 'controllers/delete.php',
             method: 'POST',
             data: { id: noteId },
             success: function () {
-                loadPage(selectedPage);
+                selectedPage = 'controllers/index.php'
+                loadPage(selectedPage)
             }
         });
     });
 
     $(document).on('click', '#search-btn', function (e) {
-        e.preventDefault();
-        const searchInput = $('#search').val();
+        e.preventDefault()
+        const searchInput = $('#search').val()
         selectedPage = 'controllers/search.php';
-        loadPage(selectedPage, 'GET', { search: searchInput });
+        loadPage(selectedPage, 'GET', { search: searchInput })
     });
 
     function loadPage(page = selectedPage, method = 'GET', data = {}) {
@@ -44,16 +45,16 @@ $(document).ready(function () {
             method: method,
             data: data,
             success: function (response) {
-                $('#content').html('');
-                $('#content').html(response);
-                setupFormHandlers();
+                $('#content').html('')
+                $('#content').html(response)
+                setupFormHandlers()
             },
         });
     }
 
     function setupFormHandlers() {
         $('#create-note').on('submit', function (e) {
-            e.preventDefault();
+            e.preventDefault()
 
             $.ajax({
                 url: 'controllers/create.php',
@@ -63,8 +64,8 @@ $(document).ready(function () {
                     selectedPage = 'controllers/index.php';
                     loadPage(selectedPage);
                 },
-            });
-        });
+            })
+        })
 
         $('#edit-note').on('submit', function (e) {
             e.preventDefault();
@@ -74,10 +75,10 @@ $(document).ready(function () {
                 method: 'POST',
                 data: $(this).serialize(),
                 success: function () {
-                    selectedPage = 'controllers/index.php';
-                    loadPage(selectedPage);
+                    selectedPage = 'controllers/index.php'
+                    loadPage(selectedPage)
                 }
-            });
-        });
+            })
+        })
     }
 })
